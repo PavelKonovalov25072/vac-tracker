0fc09820de2be7367133d09f3504855bf10b9868
Afkborn <kalaybilgehan60@gmail.com>
2022-11-13 02:11:31 +0300

    master

---
+++ app.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const reloadCommands = require("./reloadCommands");
+const { Client, GatewayIntentBits } = require("discord.js");
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === "ping") {
+    await interaction.reply("Pong!");
+  }
+});
+
+
+reloadCommands();
+client.login(process.env.DISCORD_TOKEN);
